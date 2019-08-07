afaa8786cd4b12d9122f3dc94b7f5335f1154f04
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-07 15:43:56 +0530

    Main Function is edited to implement ApplicationContext

--- SpringDemo2/src/spring/demo2/EmployeeMain.java
+++ SpringDemo2/src/spring/demo2/EmployeeMain.java
@@ -2,15 +2,19 @@ package spring.demo2;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		Resource res = new ClassPathResource("EmployeeXml.xml");
+		/*Resource res = new ClassPathResource("EmployeeXml.xml");
 		BeanFactory fac = new XmlBeanFactory(res);
-		Employee emp = (Employee)fac.getBean("employee");
+		Employee emp = (Employee)fac.getBean("employee");*/
+		ClassPathXmlApplicationContext res = new ClassPathXmlApplicationContext("EmployeeXml.xml");
+		Employee emp = (Employee)res.getBean("employee");
 		emp.DisplayInfo();
 
 	}
